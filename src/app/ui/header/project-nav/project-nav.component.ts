c24726ac1cef3b23cbe40e6536773fa19862ec58
Andrew Seguin <andrewjs@google.com>
2016-12-10 18:17:53 -0800

    fix project nav not updating active

--- src/app/ui/header/project-nav/project-nav.component.ts
+++ src/app/ui/header/project-nav/project-nav.component.ts
@@ -1,12 +1,25 @@
 import {Component, Input} from '@angular/core';
 
+export class ProjectNavLink {
+  link: string;
+  title: string;
+}
+
 @Component({
   selector: 'project-nav',
   templateUrl: './project-nav.component.html',
   styleUrls: ['./project-nav.component.scss']
 })
 export class ProjectNavComponent {
-  @Input('projectId') id;
+  projectNavLinks: ProjectNavLink[];
 
-  constructor() { }
+  @Input('projectId') set id(id: string) {
+    if (!id) { this.projectNavLinks = null; return; }
+
+    this.projectNavLinks = [
+      {link: `project/${id}/details`, title: 'Details'},
+      {link: `project/${id}/notes`, title: 'Notes'},
+      {link: `project/${id}/requests`, title: 'Requests'}
+    ];
+  }
 }
