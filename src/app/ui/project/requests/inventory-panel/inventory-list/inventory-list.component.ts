f65c708b2e2301138c451edef6d0bb5b69fb6acc
Andrew Seguin <andrewjs@google.com>
2016-12-18 21:58:33 -0800

    add component for inventory list

---
+++ src/app/ui/project/requests/inventory-panel/inventory-list/inventory-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-list',
+  templateUrl: './inventory-list.component.html',
+  styleUrls: ['./inventory-list.component.scss']
+})
+export class InventoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
