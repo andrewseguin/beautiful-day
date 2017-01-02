1330a1adada4b9b658b54a0138650d012c542ecc
Andrew Seguin <andrewjs@google.com>
2017-01-01 16:09:18 -0800

    show subheader when route changes

--- src/app/service/subheader.service.ts
+++ src/app/service/subheader.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class SubheaderService {
-  visibilitySubject: BehaviorSubject<boolean> =
-      new BehaviorSubject<boolean>(true);
+  visibilitySubject = new BehaviorSubject<boolean>(true);
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => this.visibility(true));
+  }
 
   visibility(v: boolean) {
     this.visibilitySubject.next(v);
