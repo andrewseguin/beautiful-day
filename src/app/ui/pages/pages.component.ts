c4c02b43676be41018a4ccbfedd2f90bf5b83e0d
Andrew Seguin <andrewjs@google.com>
2017-01-03 22:51:22 -0800

    scroll to top on router changes

--- src/app/ui/pages/pages.component.ts
+++ src/app/ui/pages/pages.component.ts
@@ -1,8 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, ViewChild} from "@angular/core";
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
-export class PagesComponent {}
+export class PagesComponent {
+  @ViewChild('routerContent') routerContent: ElementRef;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // When the router changes, scroll back to the top of the router content div
+        this.routerContent.nativeElement.scrollTop = 0;
+      }
+    })
+  }
+
+
+}
