4e44d3629092fdf673a0162db19698498f68ac3a
Andrew Seguin <andrewjs@google.com>
2017-01-25 13:56:41 -0800

    disable ga when not in prod

--- src/app/service/analytics.service.ts
+++ src/app/service/analytics.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
 declare let ga:Function;
 
 @Injectable()
@@ -7,10 +8,12 @@ export class AnalyticsService {
   constructor() { }
 
   sendPageview(url): void {
+    if (!environment.production) { return; }
     ga('send', 'pageview', url);
   }
 
   sendEvent(category: string, action: string, label: string, value: number): void {
+    if (!environment.production) { return; }
     ga('send', 'event', category, action, label, value);
   }
 }
