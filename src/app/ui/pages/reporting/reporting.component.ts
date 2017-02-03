2915d269a4cd1af8f8633b0f3ce0c67065dfa98e
Andrew Seguin <andrewjs@google.com>
2017-02-02 17:16:56 -0800

    add queries inputs to the report query panel

--- src/app/ui/pages/reporting/reporting.component.ts
+++ src/app/ui/pages/reporting/reporting.component.ts
@@ -4,6 +4,16 @@ import {ActivatedRoute, Params} from '@angular/router';
 import {MediaQueryService} from "../../../service/media-query.service";
 import {Request} from "../../../model/request";
 import {RequestsService} from "../../../service/requests.service";
+import {RequestSortPipe} from '../../../pipe/request-sort.pipe';
+import {Sort} from '../../shared/requests-list/requests-group/requests-group.component';
+import {ProjectsService} from '../../../service/projects.service';
+import {ItemsService} from '../../../service/items.service';
+import {Item} from '../../../model/item';
+import {Project} from '../../../model/project';
+
+export interface QueryStage {
+  querySet: string[];
+}
 
 @Component({
   selector: 'reporting',
@@ -11,12 +21,21 @@ import {RequestsService} from "../../../service/requests.service";
   styleUrls: ['./reporting.component.scss']
 })
 export class ReportingComponent implements OnInit {
+  requestSortPipe = new RequestSortPipe();
   reportId: string;
+  items: Item[] = [];
+  projects: Project[] = [];
   requests: Request[] = [];
+  filteredRequests: Request[] = [];
+  queryStages: QueryStage[] = [{querySet: ['']}];
+
+  arbitrarySort: Sort = 'request added';
 
   constructor(private route: ActivatedRoute,
               private mediaQuery: MediaQueryService,
               private requestsService: RequestsService,
+              private itemsService: ItemsService,
+              private projectsService: ProjectsService,
               private headerService: HeaderService) { }
 
   ngOnInit() {
@@ -25,11 +44,28 @@ export class ReportingComponent implements OnInit {
       this.reportId = params['id'];
     });
 
-    this.requestsService.getAllRequests()
-        .subscribe(requests => this.requests = requests);
+    this.requestsService.getAllRequests().subscribe(requests => {
+      this.requests = requests; this.performQuery();
+    });
+
+    this.itemsService.getItems().subscribe(items => {
+      this.items = items; this.performQuery();
+    });
+
+    this.projectsService.getProjects().subscribe(projects => {
+      this.projects= projects; this.performQuery();
+    });
   }
 
   hideQueries(): boolean {
     return this.mediaQuery.isMobile();
+  }
+
+  performQuery() {
+    if (!this.items || !this.projects) { return; }
+
+    this.filteredRequests = this.requests;
+    this.filteredRequests = this.requestSortPipe.transform(this.filteredRequests,
+        this.arbitrarySort, 'dumpster', this.items, this.projects)
   }
 }
