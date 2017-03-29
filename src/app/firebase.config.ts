572f9897eb3c45ab283cc10426687d1a7d8a2dca
Christian Chiama <c.chiama@icloud.com>
2017-03-30 01:18:00 +0200

    add firebase.config
    There is reference null :)

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+import { AuthMethods, AuthProviders } from 'angularfire2';
+
+export const FIREBASE_CONFIG = {
+    apiKey: 'testyAnTqAk9ECsguYTqrIU_a4869kBcoP-ahQ',
+    authDomain: 'test.firebaseapp.com',
+    databaseURL: 'https://test.firebaseio.com',
+    storageBucket: 'test.appspot.com',
+    messagingSenderId: '936740482129'
+};
+
+export const FIREBASE_AUTH = {
+    provider: AuthProviders.Google,
+    method: AuthMethods.Redirect,
+};
+
+export class FirebaseAuthentication {
+    public static CONFIG = FIREBASE_CONFIG;
+    public static AUTH_CONFIG = FIREBASE_AUTH;
+}
