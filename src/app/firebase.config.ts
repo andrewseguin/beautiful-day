7c39b5b21ae2d1155fd3d38f7fe7732ae90e0d89
Christian Chiama <c.chiama@icloud.com>
2017-03-30 02:10:21 +0200

    Create firebase.config.ts

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+import { AuthMethods, AuthProviders } from 'angularfire2';
+
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyAnTqAk9ECsguYTqrIU_a4869kBcoP-ahQ',
+    authDomain: 'wishare-dev.firebaseapp.com',
+    databaseURL: 'https://wishare-dev.firebaseio.com',
+    storageBucket: 'wishare-dev.appspot.com',
+    messagingSenderId: '936740482129'
+};
+
+export const FIREBASE_AUTH = {
+    provider: AuthProviders.Google,
+    method: AuthMethods.Redirect,
+};
+
+export class FirebaseAuthentication {
+    public static CONFIG = FIREBASE_CONFIG;
+    public static AUTH_CONFIG = FIREBASE_AUTH;
+}
