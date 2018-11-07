1ddc31eb1aae87c08298508f1adf4b9c204f924b
Andrew Seguin <andrewjs@google.com>
2018-11-06 16:14:02 -0800

    nopush login, change bg

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {Router} from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {auth} from 'firebase/app';
@@ -8,7 +8,8 @@ import {takeUntil} from 'rxjs/operators';
 @Component({
   selector: 'login',
   templateUrl: 'login.html',
-  styleUrls: ['login.scss']
+  styleUrls: ['login.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Login implements OnDestroy {
   checkingAuth = true;
@@ -16,9 +17,11 @@ export class Login implements OnDestroy {
   private destroyed = new Subject();
 
   constructor(private afAuth: AngularFireAuth,
+              private cd: ChangeDetectorRef,
               private route: Router) {
     this.afAuth.authState.pipe(takeUntil(this.destroyed)).subscribe(auth => {
       this.checkingAuth = false;
+      this.cd.markForCheck();
       if (auth) {
         let locationHash = window.location.hash.substr(1);
         this.route.navigate([locationHash || '']);
@@ -34,5 +37,6 @@ export class Login implements OnDestroy {
   login() {
     this.checkingAuth = true;
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.cd.markForCheck();
   }
 }
