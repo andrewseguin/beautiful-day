6c177dbbe2254d4a3a86f98f78d1c017ab1b9d5c
Andrew Seguin <andrewjs@google.com>
2018-11-19 21:45:58 -0800

    fix groups

--- src/app/season/admin-page/manage-groups/manage-groups.ts
+++ src/app/season/admin-page/manage-groups/manage-groups.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Group, GroupsDao} from 'app/season/dao';
+import {GroupsDao} from 'app/season/dao';
 import {Permissions} from 'app/season/services/permissions';
 import {map} from 'rxjs/operators';
 
@@ -10,16 +10,30 @@ import {map} from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManageGroups {
-  private pipeUsers = map((v: Group) => v ? v.users : []);
+  groups = this.groupsDao.map.pipe(map(groupsMap => {
+    if (!groupsMap) {
+      return [];
+    }
 
-  groups = [
-    {id: 'acquisitions', users: this.groupsDao.get('acquisitions').pipe(this.pipeUsers)},
-    {id: 'approvers', users: this.groupsDao.get('approvers').pipe(this.pipeUsers)},
-    {id: 'admins', users: this.groupsDao.get('admins').pipe(this.pipeUsers)},
-  ];
+    return [
+      {
+        id: 'acquisitions',
+        users: (groupsMap.get('acquisitions') || {}).users || []
+      },
+      {
+        id: 'approvers',
+        users: (groupsMap.get('approvers') || {}).users || []
+      },
+      {
+        id: 'admins',
+        users: (groupsMap.get('admins') || {}).users || []
+      },
+    ];
+  }));
 
   constructor(public groupsDao: GroupsDao,
-              public permissions: Permissions) {}
+              public permissions: Permissions) {
+  }
 
   update(id: string, users: string[]) {
     this.groupsDao.update(id, {users});
