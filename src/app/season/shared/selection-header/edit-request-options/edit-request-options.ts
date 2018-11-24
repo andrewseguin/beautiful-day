69d9cdbb2a9bcd7b1cbd849bd27fc5a24ba2b9c8
Andrew Seguin <andrewjs@google.com>
2018-11-24 14:44:18 -0800

    cannot delete purchased requests

--- src/app/season/shared/selection-header/edit-request-options/edit-request-options.ts
+++ src/app/season/shared/selection-header/edit-request-options/edit-request-options.ts
@@ -1,23 +1,42 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {Permissions} from 'app/season/services/permissions';
 import {ItemsDao, RequestsDao} from 'app/season/dao';
 import {Selection} from 'app/season/services';
 import {RequestDialog} from 'app/season/shared/dialog/request/request-dialog';
+import {combineLatest, Subject} from 'rxjs';
+import {startWith, take, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'edit-request-options',
   templateUrl: 'edit-request-options.html',
   styleUrls: ['edit-request-options.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditRequestOptions {
+  hasPurchased = true;
+
+  private destroyed = new Subject();
+
   constructor(private requestsDao: RequestsDao,
               private itemsDao: ItemsDao,
               private mdDialog: MatDialog,
               private selection: Selection,
+              private cd: ChangeDetectorRef,
               private requestDialog: RequestDialog,
-              public permissions: Permissions) {}
+              public permissions: Permissions) {
+    // Cannot delete requests that have already been purchased
+    this.selection.requests.changed.pipe(
+        startWith(null),
+        takeUntil(this.destroyed))
+        .subscribe(() => {
+          const requestsStream = this.selection.requests.selected.map(r => this.requestsDao.get(r));
+          combineLatest(requestsStream).pipe(take(1)).subscribe(requests => {
+            this.hasPurchased = requests.some(r => r.isPurchased);
+            this.cd.markForCheck();
+          });
+        });
+  }
 
   getSelectedRequestsCount() {
     return this.selection.requests.selected.length;
