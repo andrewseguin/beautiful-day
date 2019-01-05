e1abc7607643237618e44f7f2e9c183c60ead73d
Andrew Seguin <andrewjs@google.com>
2019-01-04 21:31:30 -0800

    allow another user; remove season selector for non admin

--- src/app/utility/valid-login.ts
+++ src/app/utility/valid-login.ts
@@ -4,5 +4,5 @@ export function isValidLogin(email: string) {
     count += email.charCodeAt(i);
   }
 
-  return count === 1694 || email.indexOf('@beautifulday.org') !== -1;
+  return count === 1694 || count === 2243 || email.indexOf('@beautifulday.org') !== -1;
 }
