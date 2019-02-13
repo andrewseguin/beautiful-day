5cbe26e769cc0622b0f66c23b47ccb4c43436858
Andrew Seguin <anlizbg@gmail.com>
2019-02-13 23:54:23 +0000

    load stocked requests

--- src/app/season/allocation-page/allocation-page.ts
+++ src/app/season/allocation-page/allocation-page.ts
@@ -1,7 +1,16 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {ItemsDao, RequestsDao, Item, Request} from 'app/season/dao';
-import {Subject, combineLatest} from 'rxjs';
-import {RequestRendererOptions} from '../services/requests-renderer/request-renderer-options';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
+import {Item, ItemsDao, Project, ProjectsDao, Request, RequestsDao} from 'app/season/dao';
+import {combineLatest, Subject} from 'rxjs';
+
+interface StockedRequest {
+  request: Request;
+  name: string;
+}
+
+interface StockedItemRequests {
+  item: Item;
+  requests: StockedRequest[];
+}
 
 @Component({
   selector: 'allocation-page',
@@ -10,25 +19,70 @@ import {RequestRendererOptions} from '../services/requests-renderer/request-rend
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AllocationPage {
+  stockedItemRequestsList: StockedItemRequests[] = [];
   private _destroyed = new Subject();
+  private projects: Map<string, Project>;
 
-  constructor (private itemsDao: ItemsDao,
-               private requestsDao: RequestsDao) {
-    combineLatest(itemsDao.list, requestsDao.list)
+  constructor(
+      itemsDao: ItemsDao, requestsDao: RequestsDao, projectsDao: ProjectsDao,
+      cd: ChangeDetectorRef) {
+    combineLatest(itemsDao.list, requestsDao.list, projectsDao.map)
         .subscribe(result => {
-          const items = result[0] as Item[];
-          const requests = result[1] as Request[];
+          const items = result[0];
+          const requests = result[1];
+          this.projects = result[2];
 
-          if (!items || !requests) {
+          if (!items || !requests || !this.projects) {
             return;
           }
 
-          // Determine the requests with stocked items and render them for allocation
-        })
-               }
+          this.stockedItemRequestsList =
+              this.getStockedItemRequestsList(result[0] || [], result[1] || []);
+          cd.markForCheck();
+        });
+  }
 
   ngOnDestroy() {
     this._destroyed.next();
     this._destroyed.complete();
+  }
+
+  getStockedItemRequestsList(items: Item[], requests: Request[]):
+      StockedItemRequests[] {
+    const stockedItems = this.getStockedItems(items);
+
+    const stockedItemRequestsMap = new Map<string, StockedItemRequests>();
+    requests.forEach(request => {
+      const item = stockedItems.get(request.item);
+      if (!item) {
+        return;
+      }
+
+      if (!stockedItemRequestsMap.get(request.item)) {
+        stockedItemRequestsMap.set(request.item, {item, requests: []});
+      }
+
+      stockedItemRequestsMap.get(request.item).requests.push({
+        request,
+        name: this.projects.get(request.project).name});
+    });
+
+    const stockedItemRequestsList = [];
+    stockedItemRequestsMap.forEach(value => {
+      stockedItemRequestsList.push(value);
+    });
+
+    return stockedItemRequestsList;
+  }
+
+  getStockedItems(items): Map<string, Item> {
+    const itemsWithStock = new Map<string, Item>();
+    items.forEach(item => {
+      if (item.quantityOwned) {
+        itemsWithStock.set(item.id, item);
+      }
+    });
+
+    return itemsWithStock;
   }
 }
