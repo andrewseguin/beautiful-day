4f7728e6376df6cfaa82a241206675c1474cad3f
Andrew Seguin <andrewjs@google.com>
2019-03-01 10:13:51 -0800

    request cost: include wear-and-tear, sales tax

--- src/app/season/utility/request-cost.ts
+++ src/app/season/utility/request-cost.ts
@@ -12,10 +12,18 @@ export function getRequestCost(itemCost: number, request: Request) {
 
   let requestCost = itemCost ? quantityToPurchase * itemCost : 0;
 
+  // Add wear-and-tear cost of item
+  if (request.allocation) {
+    requestCost += (itemCost / 5) * request.allocation;
+  }
+
+  // Adjust with manual cost adjustment
   if (request.costAdjustment) {
     requestCost += request.costAdjustment;
   }
 
+  // Add sales tax of 9.5%
+  requestCost = requestCost * 1.095;
+
   return requestCost;
 }
-
