0b34d382cf32ca38ecb1123499e950a030e36843
Andrew Seguin <andrewjs@google.com>
2022-07-01 06:54:50 -0600

    remove editable permissions from leads

--- src/app/season/services/permissions.ts
+++ src/app/season/services/permissions.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ConfigDao, Group, GroupId, GroupsDao, Project, ProjectsDao } from 'app/season/dao';
+import {
+  ConfigDao,
+  Group,
+  GroupId,
+  GroupsDao,
+  Project,
+  ProjectsDao,
+} from 'app/season/dao';
 import { containsEmail } from 'app/season/utility/contains-email';
 import { GlobalConfigDao } from 'app/service/global-config-dao';
-import {BehaviorSubject, combineLatest, Observable, Subject} from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
 import firebase from 'firebase';
 
@@ -27,7 +34,7 @@ export class Permissions {
 
   private destroyed = new Subject();
 
-  get editableProjects(): BehaviorSubject<Set<string>|null> {
+  get editableProjects(): BehaviorSubject<Set<string> | null> {
     if (!this._editableProjects) {
       this._editableProjects = new BehaviorSubject(null);
       this.watchEditableProjects();
@@ -35,31 +42,44 @@ export class Permissions {
 
     return this._editableProjects;
   }
-  _editableProjects: BehaviorSubject<Set<string|null>>;
+  _editableProjects: BehaviorSubject<Set<string | null>>;
 
-  constructor(private projectsDao: ProjectsDao,
-              private groupsDao: GroupsDao,
-              private configDao: ConfigDao,
-              private globalConfigDao: GlobalConfigDao,
-              private afAuth: AngularFireAuth) {
+  constructor(
+    private projectsDao: ProjectsDao,
+    private groupsDao: GroupsDao,
+    private configDao: ConfigDao,
+    private globalConfigDao: GlobalConfigDao,
+    private afAuth: AngularFireAuth,
+  ) {
     const changes = [
       this.groupsDao.list,
       this.afAuth.authState,
-      this.globalConfigDao.map.pipe(map(map => map ? map.get('owners').value : null)),
+      this.globalConfigDao.map.pipe(
+        map((map) => (map ? map.get('owners').value : null)),
+      ),
     ];
     this.permissions = combineLatest(changes).pipe(
-      map(result => {
+      map((result) => {
         if (result[0] && result[1] && result[2]) {
           return this.getPermissions(result[0], result[1], result[2]);
         } else {
           return null;
         }
-      }));
+      }),
+    );
 
-    this.isOwner = this.permissions.pipe(map(p => p ? p.has('owners') : null));
-    this.isAdmin = this.permissions.pipe(map(p => p ? p.has('admins') : null));
-    this.isAcquisitions = this.permissions.pipe(map(p => p ? p.has('acquisitions') : null));
-    this.isApprover = this.permissions.pipe(map(p => p ? p.has('approvers') : null));
+    this.isOwner = this.permissions.pipe(
+      map((p) => (p ? p.has('owners') : null)),
+    );
+    this.isAdmin = this.permissions.pipe(
+      map((p) => (p ? p.has('admins') : null)),
+    );
+    this.isAcquisitions = this.permissions.pipe(
+      map((p) => (p ? p.has('acquisitions') : null)),
+    );
+    this.isApprover = this.permissions.pipe(
+      map((p) => (p ? p.has('approvers') : null)),
+    );
 
     this.editableRequestProperties = {
       purchaser: this.isAcquisitions,
@@ -79,49 +99,63 @@ export class Permissions {
     const changes = [
       this.permissions,
       this.projectsDao.list,
-      this.afAuth.user.pipe(map(user => user ? user.email : '')),
-      this.configDao.map.pipe(map(values => {
-        return (values && values.has('editsDisabled')) ? values.get('editsDisabled').value : false;
-      })),
-      this.configDao.map.pipe(map(values => {
-        return (values && values.has('allLeads')) ? values.get('allLeads').value : false;
-      })),
+      this.afAuth.user.pipe(map((user) => (user ? user.email : ''))),
+      this.configDao.map.pipe(
+        map((values) => {
+          return values && values.has('editsDisabled')
+            ? values.get('editsDisabled').value
+            : false;
+        }),
+      ),
+      this.configDao.map.pipe(
+        map((values) => {
+          return values && values.has('allLeads')
+            ? values.get('allLeads').value
+            : false;
+        }),
+      ),
     ];
 
-    combineLatest(changes).pipe(takeUntil(this.destroyed)).subscribe(result => {
-      const permissions: Set<GroupId> = result[0];
-      const projects: Project[] = result[1];
-      const email: string = result[2];
-      const editsDisabled: boolean = result[3];
-      const allLeads: boolean = result[4];
+    combineLatest(changes)
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((result) => {
+        const permissions: Set<GroupId> = result[0];
+        const projects: Project[] = result[1];
+        const email: string = result[2];
+        const editsDisabled: boolean = result[3];
+        const allLeads: boolean = result[4];
 
-      // Cannot determine permissions if the user is not logged in
-      if (!permissions || !email || !projects) {
-        return;
-      }
-
-      const editableProjects = projects.filter(project => {
-        const isLead = containsEmail(project.leads, email) || allLeads;
-        const isDirector = containsEmail(project.directors, email);
-        const isAcquisitions = containsEmail(project.acquisitions, email);
-
-        let canEditRequests = isLead || isDirector || isAcquisitions;
-        if (editsDisabled && isLead) {
-          canEditRequests = containsEmail(project.whitelist, email);
+        // Cannot determine permissions if the user is not logged in
+        if (!permissions || !email || !projects) {
+          return;
         }
 
-        return canEditRequests || permissions.has('approvers');
-      });
+        const editableProjects = projects.filter((project) => {
+          const isLead = containsEmail(project.leads, email) || allLeads;
+          const isDirector = containsEmail(project.directors, email);
+          const isAcquisitions = containsEmail(project.acquisitions, email);
 
-      this._editableProjects.next(new Set(editableProjects.map(p => p.id)));
-    });
+          let canEditRequests = isDirector || isAcquisitions;
+          if (editsDisabled && isLead) {
+            canEditRequests = containsEmail(project.whitelist, email);
+          }
+
+          return canEditRequests || permissions.has('approvers');
+        });
+
+        this._editableProjects.next(new Set(editableProjects.map((p) => p.id)));
+      });
   }
 
-  getPermissions(groups: Group[], user: firebase.User, owners: string[]): Set<GroupId> {
+  getPermissions(
+    groups: Group[],
+    user: firebase.User,
+    owners: string[],
+  ): Set<GroupId> {
     const permissions = new Set<GroupId>();
 
-    groups.forEach(group => {
-      const groupUsers = group.users.map(email => email.toLowerCase().trim());
+    groups.forEach((group) => {
+      const groupUsers = group.users.map((email) => email.toLowerCase().trim());
       if (containsEmail(groupUsers, user.email)) {
         permissions.add(group.id);
       }
